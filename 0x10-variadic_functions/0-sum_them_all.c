ebc31edcdcc7f656177fcfbad55eddfb447e1767
Sheikhman_Kalderon <duah229@outlook.com>
2022-05-16 18:33:45 +0000

    My first ever written variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,31 @@
-
-#include <stdio.h>
 #include "variadic_functions.h"
+/*
+ * File: 0-sum_them_all.c
+ * Auth: Sheikhman_Kalderon
+ */
+
+
+#include <stdarg.h>
 
 /**
- * sum_them_all - Returning sum of all parameters
- * main - check the code
- * @n ...: Variable parameters to calculate sum.
- * 
- * Return: If n == 0 - 0
+ * sum_them_all - sum of all parameters.
+ * @n: parameters passed to the function.
+ * @...: variable number of parameters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         otherwise - sum all parameters
  */
-int sum_them_all(void)
+int sum_them_all(const unsigned int n, ...)
 {
-    int sum;
+	va_list numbers;
+	unsigned int index, sum = 0;
 
-    sum = sum_them_all(13, 1999, 1950);
-    printf("%d\n", sum);
-    sum = sum_them_all(13, 1999, -1000, 509);
-    printf("%d\n, sum");
-    return (0);
-    
+	va_start(numbers, n);
+
+	for (index = 0; index < n; index++)
+		sum += va_arg(numbers, int);
+
+	va_end(numbers);
+
+	return (sum);
 }
