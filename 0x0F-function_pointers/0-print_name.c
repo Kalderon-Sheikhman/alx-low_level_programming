11a3df4f188e3008d30f1f24fb3bebc298bffcca
Sheikhman_Kalderon <duah229@outlook.com>
2022-06-14 22:02:19 +0000

    Pointers in c programming

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
  * print_name - prints a name.
  * @name: input name.
@@ -8,16 +7,9 @@
  * Return: no return.
  */
 void print_name(char *name, void (*f)(char *))
-
 {
-	if (name && f)
-		f(name);
-
+	    if (name && f)
+		    f(name);
 }
-
-
-
-
-
 
 
